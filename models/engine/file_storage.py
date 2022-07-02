c2a6a61c05f7debd67e5e776b5262d66102a5007
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-07-02 16:58:20 -0500

    all

---
+++ models/engine/file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#convercion de diccionario a json.json
+import json
+import os
+from models.base_model import BaseModel
+
+class FileStorange:
+    
+    __file_path = "file.json"
+    __objects = {} 
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+    
+    def save(self):
+        pass
+    
+    def reload(self):
+        pass
