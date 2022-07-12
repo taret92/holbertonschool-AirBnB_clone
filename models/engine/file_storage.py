b0794764691fe08dafaa791464f1d55a68757f3f
taret92 <ivancho92.10@hotmail.com>
2022-07-11 20:41:51 -0500

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,17 +14,32 @@ class FileStorange:
     
     __file_path = "file.json"
     __objects = {} 
+    
+    classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+               "City": City, "Place": Place, "Review": Review, "State": State}
 
     def all(self):
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
+        """new - sets in __objects the obj"""
         if obj is not None:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
     
     def save(self):
-        pass
+        """save - writes an Object to a text file"""
+        new_dict = {}
+        with open(self.__file_path, 'w') as save:
+            for key, value in self.__objects.items():
+                new_dict[key] = value.to_dict()
+            json.dump(new_dict, save)
         
     def reload(self):
-        pass
+        """reload - deserializes the JSON file to __objects"""
+        all_obj = {}
+        if path.exists(self.__file_path):
+            all_obj = self.read_json()
+        for key, value in all_obj.items():
+            self.__objects[key] = self.classes[value['__class__']](**value)
